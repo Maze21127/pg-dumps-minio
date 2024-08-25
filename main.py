39328b0558fb21235c60aef44266bad10107f5c7
Vladimir Kul'kov <kulkov.vk@dns-shop.ru>
2024-08-25 23:53:32 +1000

    feat: add dirs cleanup

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from loguru import logger
+
 from pg_dumps_minio.exporter import Exporter
+from pg_dumps_minio.utils import cleanup_dirs
 from settings import Settings
 
 if __name__ == "__main__":
     settings = Settings()
     exporter = Exporter(settings)
-    exporter.export_all()
+    try:
+        exporter.export_all()
+    except Exception as e:
+        logger.exception(e)
+    finally:
+        cleanup_dirs(exporter.root_path)
